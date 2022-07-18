cb155b27caac70abe3afa33abb6825ac2794af2f
zairydevsinc <syed.uzair@devsinc.com>
2022-07-18 20:29:28 +0500

    Removed the errors in production Environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,8 +9,8 @@ Rails.application.configure do
     address:             'smtp.gmail.com',
     port:                 587,
     domain:               'bugzilla-devv.herokuapp.com',
-    user_name:            GMAIL_ADMIN,
-    password:             GMAIL_PASS,
+    user_name:            'syed.uzair@devsinc.com',
+    password:             'uysglcrngvpcwddb',
     authentication:       'plain',
     enable_starttls_auto: true }
 
