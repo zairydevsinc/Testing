5276e206b5a3f2524865110544c0d9b15db405cb
zairydevsinc <syed.uzair@devsinc.com>
2022-07-19 11:08:50 +0500

    change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,8 +9,8 @@ Rails.application.configure do
     address:             'smtp.gmail.com',
     port:                 587,
     domain:               'bugzilla-devv.herokuapp.com',
-    user_name:            'syed.uzair@devsinc.com',
-    password:             'uysglcrngvpcwddb',
+    user_name:            Rails.application.credentials.dig(:google_smtp, :email),
+    password:             Rails.application.credentials.dig(:google_smtp, :password),
     authentication:       'plain',
     enable_starttls_auto: true }
 
